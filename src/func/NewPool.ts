9370d06e3e905fd2b317e349fcbb5a71f74c3058
freeplanets <freeplanets@gmail.com>
2023-11-06 12:05:48 +0800

    no reget conn when error

--- src/func/NewPool.ts
+++ src/func/NewPool.ts
@@ -24,13 +24,14 @@ export default class NewPool {
 				resolve(conn);
 			}).catch(async (err) => {
 				console.log(`${caller} getConnection Error:`, err);
-				let conn: PoolConnection | undefined;
-				if (err.code === "ER_GET_CONNECTION_TIMEOUT" || err.code === "ER_CLOSING_POOL") {
-					conn = await this.resetPool();
-					resolve(conn);
-				} else {
-					resolve(undefined);
-				}
+				resolve(undefined);
+				// let conn: PoolConnection | undefined;
+				// if (err.code === "ER_GET_CONNECTION_TIMEOUT" || err.code === "ER_CLOSING_POOL") {
+				// 	conn = await this.resetPool();
+				// 	resolve(conn);
+				// } else {
+				// 	resolve(undefined);
+				// }	
 			});
 		});
 	}
